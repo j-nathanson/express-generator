6b9d8e19081a71b94ab12d31894bed17a4db53ce
jacobnathanson <jacobnath93@gmail.com>
2022-03-14 14:41:21 -0400

    File Upload

---
+++ routes/uploadRouter.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const authenticate = require('../authenticate');
+const multer = require('multer'); //middleware for multi-part form data
+
+// config storage of form data
+const storage = multer.diskStorage({
+    // where it will be stored
+    // null for no error, path
+    destination: (req, file, cb) => {
+        cb(null, 'public/images');
+    },
+    // set up name of file so they are the same on the client and backend server
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+});
+
+// file filter so uploads must be in image format
+const imageFileFilter = (req, file, cb) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        return cb(new Error('You can upload only image files!'), false);
+    }
+    // null no error, true tell multer to use the file
+    cb(null, true);
+};
+
+// create multer middleware with our configurations
+const upload = multer({ storage: storage, fileFilter: imageFileFilter });
+
+// create router
+const uploadRouter = express.Router();
+
+uploadRouter.route('/')
+    // GET not supported
+    .get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
+        res.statusCode = 403;
+        res.end('GET operation not supported on /imageUpload');
+    })
+    // POST add an image, use multer, expect a single file for uploading
+    // multer will handle errors
+    .post(authenticate.verifyUser, authenticate.verifyAdmin, upload.single('imageFile'), (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(req.file);
+    })
+    // PUT not supported
+    .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /imageUpload');
+    })
+    // DELETE not supported
+    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
+        res.statusCode = 403;
+        res.end('DELETE operation not supported on /imageUpload');
+    });
+
+module.exports = uploadRouter;
